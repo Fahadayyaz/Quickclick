83bb9b640783991ff07894023b4a817084f2c193
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-03 13:04:37 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
+import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import Splash from "./screens/Splash";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    CustomFont: require("./assets/fonts/CustomFont.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to my App!</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Splash" component={Splash} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
