c842f1ee6a00e6ff7f18a7cfea397127b392545c
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-03 12:16:53 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import Splash from "./screens/Splash";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import ForgotPassword from "./screens/ForgotPassword";
+import CreateNewPassword from "./screens/CreateNewPassword";
+import Home from "./screens/Home";
+import Submissions from "./screens/Submissions";
+import SentSuccessfully from "./screens/SentSuccessfully";
+
+import ContactUs from "./screens/ContactUs";
+import TermsAndConditions from "./screens/TermsAndConditions";
+import OtpVerification from "./screens/OtpVerification";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    Poppins_SemiBold: require("../Zyra/assets/fonts/Poppins-SemiBold.ttf"),
+    Poppins_Medium: require("../Zyra/assets/fonts/Poppins-Medium.ttf"),
+    PlusJakartaSans_Regular: require("../Zyra/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    PlusJakartaSans_Medium: require("../Zyra/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    PlusJakartaSans_Bold: require("../Zyra/assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SentSuccessfully" component={SentSuccessfully} />
+        <Stack.Screen name="Submissions" component={Submissions} />
+        <Stack.Screen name="ContactUs" component={ContactUs} />
+        <Stack.Screen
+          name="TermsAndConditions"
+          component={TermsAndConditions}
+        />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="OtpVerification" component={OtpVerification} />
+        <Stack.Screen name="CreateNewPassword" component={CreateNewPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
