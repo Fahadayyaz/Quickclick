2491dd240918bda10b49ba336f9a801c5296a4e4
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-04 16:57:03 +0500

    Create LoginSignUP.jsx

---
+++ screens/LoginSignUP.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LoginSignUP = () => {
+  return (
+    <View>
+      <Text>LoginSignUP</Text>
+    </View>
+  );
+};
+
+export default LoginSignUP;
+
+const styles = StyleSheet.create({});
