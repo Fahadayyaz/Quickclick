80c1d47b76db8df7317d398307e2b13d6e617844
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-04 13:10:16 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -6,22 +5,33 @@ import Splash from "./screens/Splash";
 import Welcome from "./screens/Welcome";
 import Home from "./screens/Home";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font"; // Importing useFonts from expo-font
 
-// Create a stack navigator
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    Poppins_SemiBold: require("./assets/fonts/Inter_ExtraBold.ttf"),
+    Poppins_Medium: require("./assets/fonts/Inter_ExtraBold.ttf"),
+    PlusJakartaSans_Regular: require("./assets/fonts/Inter_ExtraBold.ttf"),
+    PlusJakartaSans_Medium: require("./assets/fonts/Inter_ExtraBold.ttf"),
+    PlusJakartaSans_Bold: require("./assets/fonts/Inter_ExtraBold.ttf"),
+    Inter_ExtraBold: require("./assets/fonts/Inter_ExtraBold.ttf"),
+  });
+
+  if (!loaded) {
+    return null; // Show nothing while fonts are loading
+  }
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-
       <Stack.Navigator
         initialRouteName="Splash"
-        screenOptions={{ headerShown: false }} // Hides the header for all screens
+        screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Welcome" component={Welcome} />
-
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
