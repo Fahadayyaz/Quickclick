d6b0d9cbf9c8faaa86ee360a7d09dbff4dc0583a
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-04 12:01:55 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Splash from "./screens/Splash"; // Ensure this path is correct
-import Home from "./screens/Home"; // Ensure this path is correct
+import Splash from "./screens/Splash";
+import Welcome from "./screens/Welcome";
+import Home from "./screens/Home";
+import { StatusBar } from "expo-status-bar";
 
 // Create a stack navigator
 const Stack = createStackNavigator();
@@ -11,11 +13,15 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
+
       <Stack.Navigator
         initialRouteName="Splash"
         screenOptions={{ headerShown: false }} // Hides the header for all screens
       >
         <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Welcome" component={Welcome} />
+
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
