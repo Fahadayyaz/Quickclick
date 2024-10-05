1264a2587fafc2648b5b184f5eddf24996b4de69
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-05 16:10:59 +0500

    Update Home.jsx

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#1A1A1A",
   },
   text: {
     fontSize: 24,
