0d210c8ba6a9b84b24a9c82b13c2d29365204759
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-10-05 17:05:01 +0500

    Update Login.jsx

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
 
 const Login = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./../assets/themeBg.png")}
+        style={styles.bgImage}
+      >
+        <View style={styles.contentContainer}>
+          <Pressable
+            style={styles.backButton}
+            onPress={() => navigation.goBack()}
+          >
+            <Ionicons name="chevron-back" size={24} color="black" />
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  contentContainer: {
+    width: "90%",
+    height: "100%",
+    alignSelf: "center",
+    marginTop: "10%",
+    // backgroundColor: "red",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+  },
+  backButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F7F7F9",
+    width: 44,
+    height: 44,
+    borderRadius: 24,
+  },
+});
